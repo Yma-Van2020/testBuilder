47c7f35d9fac79bcbfecaa7e1a99822050dc7d0f
Katelyn Ma <74885386+Yma-Van2020@users.noreply.github.com>
2021-07-16 12:17:53 -0700

    Update detectNetwork.js

--- detectNetwork.js
+++ detectNetwork.js
@@ -7,61 +7,80 @@
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
 
-// Warning: Regular Expressions (RegEx) are NOT ALLOWED on this assignment!
-
 var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
+      // Diner's Club if Statement
+  		if ((cardNumber.substring(0, 2) === '38' || cardNumber.substring(0, 2) === '39')
+  			&& cardNumber.length === 14) {
 
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
-  if (cardNumber === undefined) {
-    return '';
+  			return 'Diner\'s Club';
+  		}
+      // American Express if Statement
+  		if ((cardNumber.substring(0, 2) === '34' || cardNumber.substring(0, 2) === '37')
+  			&& cardNumber.length === 15) {
+
+  			return 'American Express';
+  		}
+      /* Step 2
+      Visa always has a prefix of 4 and a length of 13, 16, or 19.
+      MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
+      Make sure that you continue to support Diner's Club and American Express cards. Keep testing your implementation here in the console.
+      */
+
+      //Visa if Statement
+      // excluded all prefixes from Switch 
+  if ((cardNumber.substring(0, 1) === '4' && cardNumber.substring(0, 4) !== '4903' &&
+           cardNumber.substring(0, 4) !== '4905' && cardNumber.substring(0, 4) !== '4911' &&
+           cardNumber.substring(0, 4) !== '4936')
+  			&& (cardNumber.length === 13 || cardNumber.length === 16 || cardNumber.length === 19)) {
+
+  			return 'Visa';
+  		}
+      // MasterCard if Statment
+  if ((cardNumber.substring(0, 2) === '51' || cardNumber.substring(0, 2) === '52' ||
+           cardNumber.substring(0, 2) === '53' || cardNumber.substring(0, 2) === '54' ||
+           cardNumber.substring(0, 2) === '55')
+  			&& cardNumber.length === 16) {
+
+  			return 'MasterCard';
   }
-  var inputNum = cardNumber;
-  if ((inputNum[1] === '8' || inputNum[1] === '9') && inputNum.length === 14) {
-    return 'Diner\'s Club';
-  } else if ((inputNum[1] === '4' || inputNum[1] === '7') && inputNum.length === 15) {
-    return 'American Express';
-  // eslint-disable-next-line max-len
-  } else if (inputNum[0] === '4' && (inputNum.length === 13 || inputNum.length === 16 || inputNum.length === 19)) {
-    return 'Visa';
-  } else if ((Number(inputNum.slice(0, 2)) >= 51 
-  && Number(inputNum.slice(0, 2)) <= 55) && inputNum.length === 16) {
-    return 'MasterCard';
-  } else if ((inputNum.slice(0, 4) === '6011' 
-  || (Number(inputNum.slice(0, 3)) >= 644 && Number(inputNum.slice(0, 3)) <= 649)
-  || inputNum.slice(0, 2) === '65') 
-  && (inputNum.length === 16 || inputNum.length === 19)) {
+
+      // Discover if Statement (step 3)
+  if ((cardNumber.substring(0, 4) === '6011' || cardNumber.substring(0, 3) === '644' ||
+          cardNumber.substring(0, 3) === '645' || cardNumber.substring(0, 3) === '646' ||
+          cardNumber.substring(0, 3) === '647' || cardNumber.substring(0, 3) === '648' ||
+          cardNumber.substring(0, 3) === '649' || cardNumber.substring(0, 2) === '65')
+        && (cardNumber.length === 19 || cardNumber.length === 16)) {
+
     return 'Discover';
-  } else if ((inputNum.slice(0, 4) === '5018' 
-  || inputNum.slice(0, 4) === '5020' 
-  || inputNum.slice(0, 4) === '5038'
-  || inputNum.slice(0, 4) === '6304')
-  && (inputNum.length >= 12 && inputNum.length <= 19)) {
-    return 'Maestro';
-  } else if (((Number(inputNum.slice(0, 6)) >= 622126 
-  && Number(inputNum.slice(0, 6)) <= 622925) 
-  || (Number(inputNum.slice(0, 3)) >= 624 
-  && Number(inputNum.slice(0, 3)) <= 626)
-  || (Number(inputNum.slice(0, 4)) >= 6282 
-  && Number(inputNum.slice(0, 4)) <= 6288)) 
-  && (inputNum.length === 16 
-    || inputNum.length === 18 
-    || inputNum.length === 19)) {
-    return 'China UnionPay';
-  } else if ((inputNum.slice(0, 4) === '4903' 
-    || inputNum.slice(0, 4) === '4905'
-    || inputNum.slice(0, 4) === '4911'
-    || inputNum.slice(0, 4) === '4936'
-    || inputNum.slice(0, 6) === '564182'
-    || inputNum.slice(0, 6) === '633110'
-    || inputNum.slice(0, 4) === '6333'
-    || inputNum.slice(0, 4) === '6759') 
-    && (inputNum.length === 16 
-      || inputNum.length === 18
-      || inputNum.length === 19)) {
-    return 'Switch';
   }
-};
 
+  if ((cardNumber.substring(0, 4) === '5018' || cardNumber.substring(0, 4) === '5020' ||
+          cardNumber.substring(0, 4) === '5038' || cardNumber.substring(0, 4) === '6304')
+        && (cardNumber.length >= 12 && cardNumber.length <= 19)) {
+
+    return 'Maestro';
+  }
+      // China UnionPay if Statement test
+  if (((Number(cardNumber.substring(0, 6)) >= 622126 && Number(cardNumber.substring(0, 6)) <= 622925) ||
+         (Number(cardNumber.substring(0, 3)) >= 624 && Number(cardNumber.substring(0, 3)) <= 626) ||
+         (Number(cardNumber.substring(0, 4)) >= 6282 && Number(cardNumber.substring(0, 4)) <= 6288))
+        && (cardNumber.length >= 16 && cardNumber.length <= 19)) {
+
+    return 'China UnionPay';
+  }
+       // Switch if Statement
+  if ((cardNumber.substring(0, 4) === '4903' || cardNumber.substring(0, 4) === '4905' ||
+            cardNumber.substring(0, 4) === '4911' || cardNumber.substring(0, 4) === '4936' ||
+            cardNumber.substring(0, 6) === '564182' || cardNumber.substring(0, 6) === '633110' ||
+            cardNumber.substring(0, 4) === '6333' || cardNumber.substring(0, 4) === '6759')
+           && (cardNumber.length === 16 || cardNumber.length === 18 || cardNumber.length === 19)) {
+
+    return 'Switch';
+  } else {
+  			return undefined;
+  		}
+  // Once you've read this, go ahead and try to implement this function, then return to the console.
+};
